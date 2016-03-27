b7e5c7520fd808da0ce7478d8e7eb49cde0d13ea
acm <ouyang1204@gmail.com>
2016-03-27 14:39:44 +0800

    version properties

--- src/main/java/dubbo/service/DemoServiceImpl.java
+++ src/main/java/dubbo/service/DemoServiceImpl.java
@@ -1,9 +1,13 @@
 package dubbo.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service("demoServiceImpl")
 public class DemoServiceImpl implements DemoService {
+    
+    @Value("${foo}")
+    private int foo;
 
     public void sayHelle() {
         System.out.println("hello world!");
